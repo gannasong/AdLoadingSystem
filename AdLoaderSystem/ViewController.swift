e07111b6165736238245855a6b05402b5dc67c94
Miguel <gannasong@gmail.com>
2021-09-10 15:35:02 +0800

    project init

---
+++ AdLoaderSystem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdLoaderSystem
+//
+//  Created by SUNG HAO LIN on 2021/9/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .yellow
+  }
+
+
+}
+
---
+++ AdLoaderSystemTests/AdLoaderSystemTests.swift
@@ -0,0 +1,14 @@
+//
+//  AdLoaderSystemTests.swift
+//  AdLoaderSystemTests
+//
+//  Created by SUNG HAO LIN on 2021/9/10.
+//
+
+import XCTest
+
+class AdLoaderSystemTests: XCTestCase {
+
+
+
+}
